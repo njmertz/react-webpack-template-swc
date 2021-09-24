009a6eeb0973b47557ca4233ba528361c32b8b13
Nick Mertz <nick.mertz@gmail.com>
2021-09-23 22:22:56 -0500

    Added React support.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-import './styles/index.scss';
-const elvenShieldRecipe = {
-    leatherStrips: 2,
-    ironIngot: 1,
-    refinedMoonstone: 4,
-};
+import { render } from 'react-dom';
+import App from './components/App';
 
-const elvenGauntletsRecipe = {
-    ...elvenShieldRecipe,
-    leather: 1,
-    refinedMoonstone:4,
-}
-
-console.log(elvenShieldRecipe);
-console.log(elvenGauntletsRecipe);
+render(<App />, document.getElementById('root'));
