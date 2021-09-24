2430c7d58349071bb609e86b4bdd6db9d603187b
Nick Mertz <nick.mertz@gmail.com>
2021-09-24 18:34:02 -0500

    Added image support to webpack.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import '../styles/index.scss';
 import Recipes from './Recipes';
-
+import samplepng from '../images/sample.png';
+import samplesvg from '../images/sample.svg';
 const App = () => {
     return (
         <>
           <section className="hero"></section>
           <main>
             <section>
-                <h1>Hello from React!</h1>
+                <h1>React Webpack Template SWC</h1>
             </section>
+            <img src={samplepng} alt="sample png file" width="250" />
+            <img src={samplesvg} alt="sample svg file" width="250" />
+            <Recipes />
           </main>
-          <Recipes />
         </>
     );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -12,19 +12,36 @@ if(process.env.NODE_ENV === 'production'){
 module.exports = {
   mode: mode,
   target: target,
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
   module: {
     rules: [
       {
-        test:/\.jsx?$/,
+        test: /\.(png|gif|jpe?g|svg|webp)$/i,
+        type: "asset",
+        parser:{
+          dataUrlCondition: {
+            maxSize: 200 * 1024,
+          }
+        }
+      },
+      {
+        test:/\.jsx?$/i,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
         },
       },
       {
-        test: /\.(s[ac]|c)?ss$/,
+        test: /\.(s[ac]|c)?ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            }
+          },
           'css-loader',
           'postcss-loader',
           'sass-loader',
